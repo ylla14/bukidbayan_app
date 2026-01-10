ddb2363027576999698c096405f9c29bedf1992c
ylla14 <salgadoallyssa@gmail.com>
2026-01-10 16:29:44 +0800

    added the rent in screens and bottom nav

---
+++ lib/screens/rent_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RentScreen extends StatelessWidget {
+  const RentScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
